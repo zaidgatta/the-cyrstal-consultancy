ddb772d9e4960392fc0451097efd7546382331e8
zaidgatta <135795549+zaidgatta@users.noreply.github.com>
2023-09-17 03:13:50 -0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:03357843387"><i class="fas fa-phone"></i><span>03357843387</span></a>
+         <a href="tel:03166448747"><i class="fas fa-phone"></i><span>03166448747</span></a>
+         <a href="mailto:zaidgatta3@gmail.com"><i class="fas fa-envelope"></i><span>zaidgatta3@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Karachi, Pakistan</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+
+
+</footer>
+
+<!-- footer section ends -->
